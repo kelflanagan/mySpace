f21cc2b9b0c4d1a696bd50fb3551898b2aaeef69
Kelly Flanagan <kelly_flanagan@byu.edu>
2016-09-06 18:05:50 -0600

    latest incremental changes

--- mySpace.py
+++ mySpace.py
@@ -17,7 +17,11 @@ parameters: topics, SNS topics to create
 returns: dictionary of SNS topic screen names and ARNs
 """
 def install_sns_services(topics, api_name):
-    return topics
+    topic_list = {}
+    for topic_num in topics:
+        topic_list['topic_name'] = topics[topic_num]['topic_name']
+        topic_list['display_name'] = topics[topic_num]['display_name']
+    return topic_list
 
 
 def install_aws_services(cfg, api_name):
